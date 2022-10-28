d2b75908eac6091f8191aa55578b5a14ae8d46dd
ashan <indrajee79@gmail.com>
2022-10-28 21:33:29 +0530

    i dont get the difficulty of php rendering in dev machine

--- message.php
+++ message.php
@@ -6,8 +6,14 @@
     $message = $_POST['message'];
 
     if (!empty($email) && !empty($message)){
-
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $receiver = "codingnepalweb@gmail.com";
+            $subject = "From: $name <$email>";
+            $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage:  $massage\n\nRegards,\n$name";
+        } else {
+            echo "Enter a Valid Email Address";
+        }
     } else {
-        "Email and Password Field is Required!";
+        echo "Email and Password Field is Required!";
     }
 ?>
