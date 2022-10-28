dd3e28d0bb130a97496a4ec8af859e9b6f05bf88
ashan <indrajee79@gmail.com>
2022-10-28 14:35:08 +0530

    some js

---
+++ script.js
@@ -0,0 +1,11 @@
+// lets get all required elements
+
+const form = document.querySelector("form"),
+statusTxt = form.querySelector(".button-area span");
+
+form.onsubmit = (e)=> {
+    e.preventDefault(); //preveinting form form submitting
+    statusTxt.style.display = "block";
+
+    let xhr = new XMLHttpRequest(); //creating new xml Object.
+}
